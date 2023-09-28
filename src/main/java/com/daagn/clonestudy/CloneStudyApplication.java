2c4d52128c7254a44dcb3faa612ab3fc7cd58460
jongmee <ra123221@gmail.com>
2023-09-28 21:36:44 +0900

    :tada: 프로젝트 생성하기

---
+++ src/main/java/com/daagn/clonestudy/CloneStudyApplication.java
@@ -0,0 +1,13 @@
+package com.daagn.clonestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneStudyApplication.class, args);
+	}
+
+}
