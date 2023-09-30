970af8585482b2eadf8e46ada0744442c97ab6cb
yunjeong <stac_cato@naver.com>
2023-09-30 21:16:03 +0900

    feat: member 도메인 생성

---
+++ spring-boot/src/main/java/com/ceos18/springboot/member/domain/Member.java
@@ -0,0 +1,55 @@
+package com.ceos18.springboot.member.domain;
+
+import com.ceos18.springboot.town.domain.Town;
+import jakarta.persistence.*;
+import lombok.*;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.lang.NonNull;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    private String img_url;
+
+    @NotNull
+    private String user_name;
+
+    @NotNull
+    private String user_nick;
+
+    @NotNull
+    private String pwd;
+
+    @NotNull
+    private String phone;
+
+    private String email;
+
+    @NotNull
+    private Double temp = 36.5;
+
+    @CreatedDate
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    private LocalDateTime updated_at;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "town_id", nullable = false)
+    private Town town_id;
+
+}
