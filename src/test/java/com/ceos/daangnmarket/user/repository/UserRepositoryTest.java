c509d9497726192aeb42914531b1b36a88447509
gkim <kimkm0419@naver.com>
2023-10-02 18:58:32 +0900

    feat: week2 mission additional2

--- src/test/java/com/ceos/daangnmarket/user/repository/UserRepositoryTest.java
+++ src/test/java/com/ceos/daangnmarket/user/repository/UserRepositoryTest.java
@@ -1,18 +1,16 @@
-# daangn-market
+package com.ceos.daangnmarket.user.repository;
 
-## ERD
-![image](https://github.com/CEOS-Developers/spring-daagn-market-18th/assets/68195241/79229d81-f3ef-4988-9479-0979f5d5b021)
+import com.ceos.daangnmarket.domain.user.entity.User;
+import com.ceos.daangnmarket.domain.user.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-- 장소 정보 테이블 : sido_area, sigg_area, emd_area
-- 사용자 정보 테이블 : user, activity_area
-- 사용자 관심 정보 테이블 : wish_list
-- 리뷰 (온도) 테이블 : review
-- 상품 관련 테이블 : category, product, price_offer, product_image
-- 파일 관련 테이블 : file
-- 채팅 관련 테이블 : chat_room, chat_message
+@DataJpaTest
+public class UserRepositoryTest {
 
-## Repository Test
-```java
   @Autowired
   private UserRepository userRepository;
 
@@ -29,4 +27,4 @@
     Assertions.assertThat(savedUser.getId()).isNotNull();
     Assertions.assertThat(userRepository.count()).isEqualTo(1);
   }
-```
+}
