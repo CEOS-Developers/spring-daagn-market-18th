c62e2ff3a29dafed57d3e3dc1288aa7c48e7eaa5
letskuku <yujung9288@naver.com>
2023-10-07 18:29:25 +0900

    feat: repository에서 activated 값이 true인 member 엔티티만 조회할 수 있는 메서드 추가

--- src/main/java/com/ceos18/springboot/member/repository/MemberRepository.java
+++ src/main/java/com/ceos18/springboot/member/repository/MemberRepository.java
@@ -4,6 +4,13 @@ import com.ceos18.springboot.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByIdAndActivated(Long id, Boolean activated);
+
+    List<Member> findAllByActivated(Boolean activated);
 }
