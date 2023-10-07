f3e01ee775fb100b4753a6ffbef3cfa81ac23374
younggyo <lee4519698@hanmail.net>
2023-10-07 13:09:55 +0900

    Feat : GlobalExceptionHandler

---
+++ market/src/main/java/com/ceos18/market/global/dto/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.ceos18.market.global.dto;
+
+import lombok.Builder;
+
+public class ErrorResponse {
+    private int statusCode;
+    private String message;
+
+    public ErrorResponse(int statusCode, String message) {
+        this.statusCode = statusCode;
+        this.message = message;
+    }
+}
---
+++ market/src/main/java/com/ceos18/market/global/dto/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.ceos18.market.global.dto;
+
+import com.ceos18.market.domain.product.exception.ProductNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleProductNotFoundException(ProductNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+}
