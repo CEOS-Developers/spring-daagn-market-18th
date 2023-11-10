b4b67714f557df7244ba9e6116e7d77f93686cb8
aeyongdodam <kikititi0510@gmail.com>
2023-11-11 01:15:02 +0900

    refactor: 안쓰는 import 제거

--- clone-coding/src/main/java/com/carrot/clonecoding/chat/domain/model/ChatMessage.java
+++ clone-coding/src/main/java/com/carrot/clonecoding/chat/domain/model/ChatMessage.java
@@ -4,8 +4,6 @@ package com.carrot.clonecoding.chat.domain.model;
 import com.carrot.clonecoding.common.base.BaseTimeEntity;
 import com.carrot.clonecoding.common.enums.MessageStatus;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
