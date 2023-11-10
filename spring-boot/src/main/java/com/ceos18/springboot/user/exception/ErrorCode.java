3efedb150c42f021f9816e659d836ddd8b040cd5
yunjeong <stac_cato@naver.com>
2023-11-11 02:24:25 +0900

    feat: spring security, jwt 로그인 사용 시 발생하는 exception 관련 handler 추가

--- spring-boot/src/main/java/com/ceos18/springboot/user/exception/ErrorCode.java
+++ spring-boot/src/main/java/com/ceos18/springboot/user/exception/ErrorCode.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
     USEREMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, ""),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "");
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, ""),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "토큰이 존재하지 않습니다.");
 
     private HttpStatus httpStatus;
     private String message;
