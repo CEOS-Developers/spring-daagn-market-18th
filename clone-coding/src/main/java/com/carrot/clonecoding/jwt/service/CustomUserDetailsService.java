42ce8a36b2a8205b9ad5046774e1ad7e849ae9b1
aeyongdodam <kikititi0510@gmail.com>
2023-11-11 01:18:45 +0900

    feat: jwt 로직 생성

---
+++ clone-coding/src/main/java/com/carrot/clonecoding/jwt/service/CustomUserDetailsService.java
@@ -0,0 +1,42 @@
+package com.carrot.clonecoding.jwt.service;
+
+import com.carrot.clonecoding.user.domain.model.User;
+import com.carrot.clonecoding.user.domain.repository.UserRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component("userDetailsService")
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String nickName) {
+        return userRepository.findByNickName(nickName)
+                .map(user -> createUser(nickName, user))
+                .orElseThrow(() -> new UsernameNotFoundException("Not found" + nickName));
+    }
+
+    private org.springframework.security.core.userdetails.User createUser(String username, User user) {
+        if (!user.isActivated()) {
+            throw new RuntimeException(username + " -> 활성화되어 있지 않습니다.");
+        }
+
+        List<GrantedAuthority> grantedAuthorities = user.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
+                .collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(user.getNickName(),
+                user.getPassword(),
+                grantedAuthorities);
+    }
+}
