5f1ab7140fb44099fd30de55bc407156f5bdf381
aeyongdodam <kikititi0510@gmail.com>
2023-11-11 01:18:13 +0900

    feat: Authority entity 생성

---
+++ clone-coding/src/main/java/com/carrot/clonecoding/auth/domail/model/Authority.java
@@ -0,0 +1,20 @@
+package com.carrot.clonecoding.auth.domail.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class Authority {
+    @Id
+    @Column(name = "authority_name", length = 50)
+    private String authorityName;
+}
