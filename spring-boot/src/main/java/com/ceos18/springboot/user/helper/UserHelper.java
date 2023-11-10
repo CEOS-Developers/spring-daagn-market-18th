a1af31d6053a01f9bce1506662952f3c464dafda
yunjeong <stac_cato@naver.com>
2023-11-10 16:21:54 +0900

    refact: UserService 로그인 코드 UserHelper로 분리

---
+++ spring-boot/src/main/java/com/ceos18/springboot/user/helper/UserHelper.java
@@ -0,0 +1,27 @@
+package com.ceos18.springboot.user.helper;
+
+import com.ceos18.springboot.user.domain.User;
+import com.ceos18.springboot.user.exception.AppException;
+import com.ceos18.springboot.user.exception.ErrorCode;
+import com.ceos18.springboot.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UserHelper {
+    public final UserRepository userRepository;
+
+    public User findByEmail(String email) {
+        return userRepository
+                .findByEmail(email)
+                .orElseThrow(
+                        () -> new AppException(ErrorCode.USEREMAIL_NOT_FOUND, "존재하지 않는 이메일입니다."));
+    }
+
+    public void validatePwd(User selectedUser, String pwd) {
+        if(!pwd.equals(selectedUser.getPwd())){
+            throw new AppException(ErrorCode.INVALID_PASSWORD, "잘못된 비밀번호입니다.");
+        }
+    }
+}
--- spring-boot/src/main/java/com/ceos18/springboot/user/service/UserService.java
+++ spring-boot/src/main/java/com/ceos18/springboot/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.ceos18.springboot.user.service;
 import com.ceos18.springboot.user.domain.User;
 import com.ceos18.springboot.user.exception.AppException;
 import com.ceos18.springboot.user.exception.ErrorCode;
+import com.ceos18.springboot.user.helper.UserHelper;
 import com.ceos18.springboot.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,17 +12,14 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;
+    private final UserHelper userHelper;
 
     public String login(String email, String pwd){
         // user Email 없을 때
-        User selectedUser = userRepository.findByEmail(email)
-                .orElseThrow(() -> new AppException(ErrorCode.USEREMAIL_NOT_FOUND, "등록되지 않은 이메일입니다."));
+        User selectedUser = userHelper.findByEmail(email);
 
         // password 잘못 되었을 때
-        if(!pwd.equals(selectedUser.getPwd())){
-            throw new AppException(ErrorCode.INVALID_PASSWORD, "잘못된 비밀번호입니다.");
-        }
+        userHelper.validatePwd(selectedUser, pwd);
 
         return "token";
     }
