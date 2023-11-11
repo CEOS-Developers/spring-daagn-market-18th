bab169cbad6d25bb610ea29d2544a3654d890881
jongmee <ra123221@gmail.com>
2023-11-11 21:20:56 +0900

    :sparkles: Member Entity가 UserDetails를 구현하기

--- src/main/java/com/daagn/clonestudy/member/domain/Member.java
+++ src/main/java/com/daagn/clonestudy/member/domain/Member.java
@@ -2,16 +2,24 @@ package com.daagn.clonestudy.member.domain;
 
 import com.daagn.clonestudy.common.BaseTimeEntity;
 import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @NoArgsConstructor
-public class Member extends BaseTimeEntity {
+public class Member extends BaseTimeEntity implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,6 +30,9 @@ public class Member extends BaseTimeEntity {
 
   @Column(nullable = false)
   private String phoneNumber;
+
+  @Column(nullable = false)
+  private String password;
 
   @Column(nullable = false)
   private String town;
@@ -47,6 +58,47 @@ public class Member extends BaseTimeEntity {
     this.temperature = 36.5;
     this.redeal = 0;
     this.responseRate = 0;
+  }
+
+  @ElementCollection
+  @Builder.Default
+  private List<String> roles = new ArrayList<>();
+
+  @Override
+  public String getUsername() {
+    return phoneNumber;
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return this.roles.stream()
+        .map(SimpleGrantedAuthority::new)
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
   }
 
 }
