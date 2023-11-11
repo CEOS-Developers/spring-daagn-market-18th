18d8f7be8b6964574990a977222ac7bab0dd02d9
jongmee <ra123221@gmail.com>
2023-11-11 21:25:04 +0900

    :sparkles: Username을 전화번호로하여(ID 역할) UserDetails 반환하기

--- src/main/java/com/daagn/clonestudy/member/domain/MemberRepository.java
+++ src/main/java/com/daagn/clonestudy/member/domain/MemberRepository.java
@@ -1,8 +1,9 @@
 package com.daagn.clonestudy.member.domain;
 
 import com.daagn.clonestudy.member.domain.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    Optional<Member> findByPhoneNumber(String phoneNumber);
 }
